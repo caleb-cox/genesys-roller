9243f6378f9288b95119da7a97b76e1b29406322
caleb-cox <cacox25@gmail.com>
2022-02-18 11:28:11 -0500

    Remove rolling logic from die component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import GenesysDie from "./GenesysDie";
 import {
@@ -11,15 +11,53 @@ import {
 } from "./constants";
 
 const App = () => {
+  const [dicePool, setDicePool] = useState([
+    {
+      type: ABILITY,
+      value: Math.random()
+    },
+    {
+      type: PROFICIENCY,
+      value: Math.random()
+    },
+    {
+      type: BOOST,
+      value: Math.random()
+    },
+    {
+      type: DIFFICULTY,
+      value: Math.random()
+    },
+    {
+      type: CHALLENGE,
+      value: Math.random()
+    },
+    {
+      type: SETBACK,
+      value: Math.random()
+    },
+  ]);
+  
+  const removeDieByIndex = (index) => {
+    return () => setDicePool((prevState) => {
+      console.log(dicePool)
+      const newState = [...prevState];
+      newState.splice(index, 1);
+      return newState;
+    });
+  };
+
   return (
-    <>
-      <GenesysDie type={ABILITY}/>
-      <GenesysDie type={PROFICIENCY}/>
-      <GenesysDie type={BOOST}/>
-      <GenesysDie type={DIFFICULTY}/>
-      <GenesysDie type={CHALLENGE}/>
-      <GenesysDie type={SETBACK}/>
-    </>
+    <div style={{display: "flex", margin: "50px"}}>
+      {dicePool.map(({ type, value }, index) => (
+        <GenesysDie
+          key={index}
+          type={type}
+          value={value}
+          onClick={removeDieByIndex(index)}
+        />
+      ))}
+    </div>
   );
 };
 
--- src/GenesysDie.jsx
+++ src/GenesysDie.jsx
@@ -1,23 +1,25 @@
-import React, { useState } from "react";
 import classNames from "classnames";
 
 import "./GenesysDie.scss";
 import { diceMap } from "./constants";
 
-const GenesysDie = ({ type }) => {
-  const [results, setResults] = useState([]);
-  const resultMap = diceMap[type];
-
-  const roll = () => {
-    const sides = Object.keys(resultMap).length;
-    const side = Math.floor(Math.random() * sides + 1);
-    setResults(resultMap[side]);
+const GenesysDie = ({ type, value, onClick }) => {
+  const getSymbols = () => {
+    const symbolMap = diceMap[type];
+    const sides = Object.keys(symbolMap).length;
+    const side = Math.floor(value * sides + 1);
+    return symbolMap[side];
   };
 
   return (
-    <div className={classNames("GenesysDie", type)} onClick={roll}>
-      {results.map((result, index) => (
-        <div className="symbol" key={index}>{result}</div>
+    <div
+      className={classNames("GenesysDie", type)}
+      onClick={onClick}
+    >
+      {getSymbols().map((symbol, index) => (
+        <div className="symbol" key={index}>
+          {symbol}
+        </div>
       ))}
     </div>
   );
