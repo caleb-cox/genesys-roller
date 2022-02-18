af5170824b10b623d2fb86a28d5ed91dc081770e
caleb-cox <cacox25@gmail.com>
2022-02-18 14:10:48 -0500

    Replace single quotes with double quotes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import "normalize.css/normalize.css";
 import "the-new-css-reset/css/reset.css";
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App'
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./components/App"
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 )
