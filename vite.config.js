c9025c4ee5c91f70cea62498946d109b265a3c58
caleb-cox <cacox25@gmail.com>
2022-02-20 20:35:07 -0500

    Add server config required for glitch deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  // server: {
+  //   strictPort: true,
+  //   hmr: {
+  //     port: 443 // Run the websocket server on the SSL port
+  //   }
+  // }
 })
