909a18b615fb8f8a2605b00f030dd6d6d2b615c6
caleb-cox <cacox25@gmail.com>
2022-02-21 11:43:59 -0500

    Turn Symbols into spans

--- src/components/Symbol.jsx
+++ src/components/Symbol.jsx
@@ -3,7 +3,7 @@ import "/src/styles/Symbol.scss";
 import { SYMBOL_MAP } from "/src/constants";
 
 const Symbol = ({ type }) => {
-  return <div className={`Symbol ${type}`}>{SYMBOL_MAP[type]}</div>;
+  return <span className={`Symbol ${type}`}>{SYMBOL_MAP[type]}</span>;
 };
 
 export default Symbol;
